ce67f5926a6b22c005ec1fbd4078b01ca8b7b7ff
Ayush <ayushgupta141001@gmail.com>
2022-06-27 22:34:58 +0530

    made few changes

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+
+// Back To Top 
+var mybutton = document.querySelector(".to-top")
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        mybutton.style.display = "block";
+    } else {
+        console.log(document.body.scrollTop)
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
